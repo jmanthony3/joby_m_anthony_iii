b932d9ef5e4b4a2e45a624f041fed47c6afa0237
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2022-07-18 14:57:44 -0400

    rebase of repo from LFS

---
+++ compile_and_upload.bat
@@ -0,0 +1,32 @@
+@echo off
+type nul > temp
+sed -n "s%%[[:blank:]]*version = %%%%p" "pyproject.toml" | sed -n "1p" > temp
+for /f %%i in ('type "temp"') do (
+    set version=%%i
+)
+del temp
+set version=%version:~1,-1%
+
+del src\joby_m_anthony_iii\numerical_methods.log
+del tests\numerical_methods.log
+
+pip install --upgrade build twine
+python -m build
+twine check ./dist/joby_m_anthony_iii-%version%*
+
+
+python -m twine upload --repository pypi ./dist/joby_m_anthony_iii-%version%.tar.gz
+timeout 5 /NOBREAK
+pip install --upgrade joby_m_anthony_iii==%version%
+
+echo "Compiled, uploaded, and updated to 'joby_m_anthony_iii-%version%'. Generating documentation..."
+
+rmdir /Q /S docs
+sphinx-apidoc -f -M -F -H="joby_m_anthony_iii" -A="Joby M. Anthony III" -V="%version%" -o ./docs ./src/joby_m_anthony_iii
+sed -n "/# -- Extension configuration /{=}" "docs/conf.py" | sed -n "1p" > temp
+for /f %%i in ('type "temp"') do (
+    set /A line_number=%%i+1
+)
+del temp
+sed -i "%line_number%s/.*/extensions = [\n    'sphinx.ext.napoleon'\n]\nnapoleon_google_docstring = False\n/" "docs/conf.py"
+sphinx-build -b html docs/ docs/_build/html
---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,21 @@
+.. joby_m_anthony_iii documentation master file, created by
+   sphinx-quickstart on Mon Jul 18 10:17:08 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to joby_m_anthony_iii's documentation!
+==============================================
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+
+   joby_m_anthony_iii
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
